4c01f5550905889b0e6464453e31927d6e7e8281
MIGUEL_JARED_RENTERIA_MARRUFO <a.21308051281099@cbtis128.edu.mx>
2024-03-11 15:23:10 +0000

    Actualizado

---
+++ lib/Pantalla1_1099.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Pantalla1_1099 extends StatelessWidget {
+  const Pantalla1_1099({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Pantalla1_Renteria1099"),
+          backgroundColor: Color(0xff741d1d),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                child: Text("Renteria Ejemplo"),
+              )
+            ], //ninios
+          ),
+        ));
+  }
+}
---
+++ lib/PantallaInicial_1099.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PantallaInicial_1099 extends StatelessWidget {
+  const PantallaInicial_1099({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Pantalla Inicial Renteria1099"),
+          backgroundColor: Color(0xff5e07ff),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantall1_1099");
+              },
+              child: Text("mover a pantalla1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantall2_1099");
+              },
+              child: Text("mover a pantalla2"),
+            )
+          ], //niniosn
+        )));
+  }
+}
